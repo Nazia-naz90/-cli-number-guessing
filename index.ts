fce97f5a3362668488fe6ceb24c70667731c760a
Nazia-naz90 <161705774+Nazia-naz90@users.noreply.github.com>
2024-03-25 14:50:16 -0700

    Created number guessing game.

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1 # computer will generate a random number //
+
+// 2 # user input the guessing number //
+
+// 3 # compare user number with computer generated number//
+
+const randomNumber=Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer . prompt([
+   { name:"userGuessedNumber",
+    type:"number",
+    message:"please guess a number between 1-6: ",
+},
+]);
+
+if(answer.userGuessedNumber === randomNumber ){
+    console.log("congratulation ! You guessed right number.");
+
+}else{
+    console.log("You guessed wrong number");
+}
